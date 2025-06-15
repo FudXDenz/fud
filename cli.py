a57927c448476251fac419d64d36568a540f181b
FudXDenz <162475721+FudXDenz@users.noreply.github.com>
2025-06-15 08:12:00 +0000

    Add basic argparse parser to cli.py

--- cli.py
+++ cli.py
@@ -1,1 +1,12 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Note Manager CLI')
+    parser.add_argument('--add', help='Add a note')
+    parser.add_argument('--list', action='store_true', help='List notes')
+    parser.add_argument('--delete', type=int, help='Delete note by ID')
+    args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()
 
